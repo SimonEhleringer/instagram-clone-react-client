32e7d81bb92c827c0a64bd5c8c1cd68ef3011b3b
Simon Ehleringer <simonehleringer@gmail.com>
2021-05-08 08:49:06 +0200

    Finish design of login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,39 +1,42 @@
-import React from "react";
-import "./LoginPage.scss";
-import instagramLogo from "../instagram-logo.png";
+import React from 'react';
+import './LoginPage.scss';
+import instagramLogo from '../instagram-logo.png';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   return (
-    // <div className='login-form-wrapper'>
-    //   <div className='login-form'>
-    //     <div className='login-form__box'>
-    //       <img src={instagramLogo} alt='Instagram' />
-    //       <input />
-    //       <input />
-    //     </div>
-    //   </div>
-
-    //   <div className='login-form__box'>Du hast kein Konto? Registrieren</div>
-    // </div>
-    <div className="login-form-wrapper">
-      <div className="login-form">
-        <div className="login-form__box">
+    <div className='login-form-wrapper'>
+      <div className='login-form'>
+        <div className='login-form__box'>
           <img
-            className="login-form__logo"
+            className='login-form__logo'
             src={instagramLogo}
-            alt="Instagram"
+            alt='Instagram'
           />
 
-          <div className="login-form__content-wrapper">
-            <input></input>
-            <input></input>
-            <div className="login-form__button-wrapper">
-              <button className="login-form__button">Anmelden</button>
+          <div className='login-form__content-wrapper'>
+            <input
+              className='login-form__input'
+              placeholder='Benutzername oder E-Mail'
+            />
+            <input className='login-form__input' placeholder='Passwort' />
+            <div className='login-form__button-wrapper'>
+              <button className='login-form__button'>Anmelden</button>
             </div>
           </div>
         </div>
 
-        <div className="login-form__box">Du hast kein Konto? Registrieren</div>
+        <div className='login-form__box'>
+          <div className='login-form__redirect-register__text'>
+            Du hast kein Konto?{' '}
+            <Link
+              className='login-form__redirect-register__link'
+              to='/register'
+            >
+              Registrieren
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
