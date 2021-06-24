3fd5931d87b98fec04e7331994bb4c778eba10b9
SimonEhleringer <simon.ehleringer@tuer.de>
2021-06-24 13:00:00 +0200

    Add creation time label to post

--- src/feed/FeedPost/index.tsx
+++ src/feed/FeedPost/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { FeedPostResponseDto } from '../../api/meFeed';
-import Avatar from '../../shared/Avatar';
-import { Image, Transformation } from 'cloudinary-react';
-import './style.scss';
-import UserProfileLink from '../../shared/UserProfileLink';
-import { calculateDisplayTime } from '../../shared/time';
+import React from "react";
+import { FeedPostResponseDto } from "../../api/meFeed";
+import Avatar from "../../shared/Avatar";
+import { Image, Transformation } from "cloudinary-react";
+import "./style.scss";
+import UserProfileLink from "../../shared/UserProfileLink";
+import { calculateDisplayTime, getDisplayTimeString } from "../../shared/time";
 
 export interface FeedPostProps {
   feedPost: FeedPostResponseDto;
@@ -14,8 +14,8 @@ const FeedPost: React.FC<FeedPostProps> = ({ feedPost }) => {
   console.log(calculateDisplayTime(feedPost.creationTime));
 
   return (
-    <div className='feed-post'>
-      <div className='feed-post__top'>
+    <div className="feed-post">
+      <div className="feed-post__top">
         <Avatar
           username={feedPost.creator.username}
           widthInPx={32}
@@ -25,19 +25,23 @@ const FeedPost: React.FC<FeedPostProps> = ({ feedPost }) => {
         <UserProfileLink user={feedPost.creator} />
       </div>
 
-      <div className='feed-post__middle'>
+      <div className="feed-post__middle">
         <Image publicId={feedPost.publicImageId} alt={feedPost.publicImageId}>
-          <Transformation crop='lfill' width={800} />
+          <Transformation crop="lfill" width={800} />
         </Image>
       </div>
 
-      <div className='feed-post__bottom'>
+      <div className="feed-post__bottom">
         {feedPost.text && (
-          <>
-            <UserProfileLink user={feedPost.creator} />{' '}
+          <div className="feed-post__caption">
+            <UserProfileLink user={feedPost.creator} />{" "}
             <span>{feedPost.text}</span>
-          </>
+          </div>
         )}
+
+        <div className="feed-post__creation-time">
+          vor {getDisplayTimeString(feedPost.creationTime)}
+        </div>
       </div>
     </div>
   );
--- src/shared/time.ts
+++ src/shared/time.ts
@@ -1,17 +1,9 @@
-// TODO: convert utc to my time
-
+// TODO: Write tests
 export const calculateDisplayTime = (date: Date) => {
-  const now = new Date();
+  const now = new Date(
+    new Date().getTime() + new Date().getTimezoneOffset() * 60000
+  );
   const nowAsMilliseconds = Date.parse(now.toUTCString());
-  // const nowAsMilliseconds = Date.UTC(
-  //   now.getUTCFullYear(),
-  //   now.getUTCMonth(),
-  //   now.getUTCDate(),
-  //   now.getUTCHours(),
-  //   now.getUTCMinutes(),
-  //   now.getUTCSeconds(),
-  //   now.getUTCMilliseconds()
-  // );
 
   const dateAsMilliseconds = new Date(date).getTime();
 
@@ -31,29 +23,58 @@ export const calculateDisplayTime = (date: Date) => {
     yearsDiff * 12 + (new Date(date).getMonth() - now.getMonth());
 
   const diffs: Diff[] = [
-    { diff: yearsDiff, diffType: 'years' },
-    { diff: monthsDiff, diffType: 'months' },
-    { diff: weeksDiff, diffType: 'weeks' },
-    { diff: daysDiff, diffType: 'days' },
-    { diff: hoursDiff, diffType: 'hours' },
-    { diff: minutesDiff, diffType: 'minutes' },
+    { diff: yearsDiff, diffType: "years" },
+    { diff: monthsDiff, diffType: "months" },
+    { diff: weeksDiff, diffType: "weeks" },
+    { diff: daysDiff, diffType: "days" },
+    { diff: hoursDiff, diffType: "hours" },
+    { diff: minutesDiff, diffType: "minutes" },
   ];
 
   let diffToReturn = diffs.find((val) => val.diff > 0);
 
   if (!diffToReturn) {
-    diffToReturn = { diff: 1, diffType: 'minutes' };
+    diffToReturn = { diff: 1, diffType: "minutes" };
   }
 
   return diffToReturn;
 };
 
+// TODO: Add tests
+export const getDisplayTimeString = (date: Date) => {
+  const map = new Map<diffType, { singular: string; plural: string }>();
+
+  map.set("years", { singular: "Jahr", plural: "Jahre" });
+  map.set("months", { singular: "Monat", plural: "Monate" });
+  map.set("weeks", { singular: "Woche", plural: "Wochen" });
+  map.set("days", { singular: "Tag", plural: "Tage" });
+  map.set("hours", { singular: "Stunde", plural: "Stunden" });
+  map.set("minutes", { singular: "Minute", plural: "Minuten" });
+
+  const displayTimeObj = calculateDisplayTime(date);
+
+  const unitStringObj = map.get(displayTimeObj.diffType);
+
+  if (!unitStringObj) {
+    throw new Error(
+      `No matching unit string could be found for "${displayTimeObj.diffType}"`
+    );
+  }
+
+  const unitString =
+    displayTimeObj.diff !== 1 ? unitStringObj.plural : unitStringObj.singular;
+
+  const displayString = `${displayTimeObj.diff} ${unitString}`;
+
+  return displayString;
+};
+
 type Diff = { diff: number; diffType: diffType };
 
 export type diffType =
-  | 'years'
-  | 'months'
-  | 'weeks'
-  | 'days'
-  | 'hours'
-  | 'minutes';
+  | "years"
+  | "months"
+  | "weeks"
+  | "days"
+  | "hours"
+  | "minutes";
